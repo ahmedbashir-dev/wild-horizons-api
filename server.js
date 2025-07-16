26858e1bcc6cc1f253b698b759e81579ec552773
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-07-17 02:24:11 +0500

    Serving JSON data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import http from 'node:http';
+import { getDataFromDb } from './database/db.js';
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-    res.end('Hello from the server')
+
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDb();
+    console.log(destinations, req.url, req.method);
+    if(req.url === '/api' && req.method==='GET'){
+        res.end(JSON.stringify(destinations))
+    }
 });
 
 server.listen(PORT, ()=>console.log(`server is running on port: ${PORT}`));
